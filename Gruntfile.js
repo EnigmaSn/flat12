f505daa7492b19d4fe5c0a303098387521f9835e
EnigmaSn <enigmasnape@gmail.com>
2016-11-29 20:28:13 +0300

    автоматизация

---
+++ Gruntfile.js
@@ -0,0 +1,150 @@
+"use strict";
+
+module.exports = function (grunt) {
+	require("load-grunt-tasks")(grunt);
+
+	grunt.initConfig({
+		clean: {
+			build: ["../build"],
+			release: [
+				'../build/css/*.css', '!../build/css/*.min.css'
+			],
+			options: {
+				force: true
+			}
+		},
+
+		copy: {
+			build: {
+				files: [
+					{
+						expand: true,
+						src: [
+							"fonts/**", "img/**"
+						],
+						dest: "../build"
+					}
+				]
+			},
+			html: {
+				files: [
+					{
+						expand: true,
+						src: ["*.html"],
+						dest: "../build"
+					}
+				]
+			},
+			scripts: {
+				files: [
+					{
+						expand: true,
+						src: ["js/**"],
+						dest: "../build"
+					}
+				]
+			}
+		},
+
+		csso: {
+			style: {
+				options: {
+					report: "gzip"
+				},
+				files: {
+					"../build/css/style.min.css": ["../build/css/style.css"],
+					"../build/css/bootstrap.min.css": ["../build/css/bootstrap.css"]
+				}
+			}
+		},
+
+		imagemin: {
+			images: {
+				options: {
+					optimizationLevel: 3
+				},
+				files: [
+					{
+						expand: true,
+						src: ["../build/img/**"]
+					}
+				]
+			}
+		},
+
+		less: {
+			style: {
+				files: {
+					"../build/css/style.css": ["less/style.less"],
+					"../build/css/bootstrap.css": ["less/bootstrap.less"]
+				}
+			}
+		},
+
+		postcss: {
+			options: {
+				processors: [
+					require("autoprefixer")({
+						browsers: ["last 1 version", "last 2 Chrome versions", "last 2 Firefox versions", "last 2 Opera versions", "last 2 Edge versions"]
+					}),
+					require("css-mqpacker")({sort: true})
+				]
+			},
+			style: {
+				src: "../build/css/*.css"
+			}
+		},
+
+		browserSync: {
+			server: {
+				bsFiles: {
+					src: ["../build/*.html", "../build/css/*.css", "../build/js/*.js"]
+				},
+				options: {
+					server: "../build",
+					watchTask: true,
+					notify: false,
+					open: true,
+					ui: false
+				}
+			}
+		},
+
+		watch: {
+			html: {
+				files: ["*.html"],
+				tasks: ["copy:html"],
+				options: {
+					spawn: false
+				}
+			},
+			scripts: {
+				files: ["js/*.js"],
+				tasks: ["copy:scripts"],
+				options: {
+					spawn: false
+				}
+			},
+			style: {
+				files: ["less/**/*.less"],
+				tasks: [
+					"less", "postcss", "csso"
+				],
+				options: {
+					spawn: false
+				}
+			}
+		}
+	});
+
+	grunt.registerTask("serve", ["browserSync", "watch"]);
+	grunt.registerTask("build", [
+		"clean:build",
+		"copy",
+		"less",
+		"postcss",
+		"csso",
+		"imagemin",
+		"clean:release"
+	]);
+};
